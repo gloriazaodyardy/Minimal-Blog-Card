26472d4f7cfd4d28cc8337e336df2034e98351d7
Gloriaza <gloriazayemima@gmail.com>
2025-07-02 19:45:19 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <div class="card">
+      <div class="card__tag"></div>
+      <div class="card__content">
+        <h1>Embracing Minimalism</h1>
+        <p>From minimalist sculptures to minimalist paintings, this blog will inspire you to appreciate the beauty that
+          lies in simplicity.</p>
+      </div>
+      <p class="card__author">
+        Annie Spratt
+      </p>
+    </div>
+  </main>
+  <footer>
+    <p>
+      Coded by <a href="#">Gloriaza</a> | Challenge by
+      <a href="https://www.devchallenges.io?ref=challenge" target="_blank">devChallenges.io</a>.
+    </p>
+  </footer>
+</template>
+
+<style scoped></style>
