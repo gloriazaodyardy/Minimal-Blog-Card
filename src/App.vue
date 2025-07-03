79c8632d9596a9fa67ca2e9bf9953a11567c3361
Gloriaza <gloriazayemima@gmail.com>
2025-07-04 00:11:53 +0700

    Improves card layout and spacing

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
-  <header class="my-11"></header>
+  <header class="my-12"></header>
   <main>
-    <div class="flex flex-col max-w-[21rem] rounded-2xl shadow-card bg-white">
-      <img class="rounded-3xl p-4" src="../resources/Cactus@2x.jpg" alt="Cactus image" srcset="../resources/Cactus.jpg">
-      <div class="flex flex-col mb-4 mx-4 bg-purple-soft py-1 w-16 rounded-xl">
+    <div class="flex flex-col rounded-2xl shadow-card bg-white">
+      <img class="max-w-[21rem] max-h-[9.5rem] rounded-3xl p-4" src="../resources/Cactus@2x.jpg" alt="Cactus image" srcset="../resources/Cactus.jpg">
+      <div class="flex flex-col mb-4 mx-4 bg-purple-soft py-1 px-4 w-16 rounded-xl">
         <b class="text-purple text-tag text-center">Design</b>
       </div>
       <div class="w-full">
