11c5742fa351c551b7a2b9a8537613a5b0021758
Gloriaza <gloriazayemima@gmail.com>
2025-07-03 13:57:04 +0700

    Improves blog card padding, height, width, sizing

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 <div align="center">
   <h3>
-    <a href="{https://your-demo-link.your-domain}">
+    <a href="https://gloriazaodyardy.github.io/Minimal-Blog-Card/">
       Demo
     </a>
     <span> | </span>
--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
 </script>
 
 <template>
-  <header></header>
+  <header class="py-[164px]"></header>
   <main>
     <div class="flex flex-col p-4 max-w-[21rem] rounded-2xl gap-4 shadow-card">
-      <img class="rounded-lg" src="../resources/Cactus.jpg" alt="Cactus image" srcset="../resources/Cactus@2x.jpg">
-      <div class="flex flex-col items-center bg-purple-soft py-1 w-14 rounded-xl text-center">
+      <img class="rounded-lg" src="../resources/Cactus@2x.jpg" alt="Cactus image" srcset="../resources/Cactus.jpg">
+      <div class="flex flex-col items-center bg-purple-soft py-1 w-16 rounded-xl text-center">
         <b class="text-purple text-tag">Design</b>
       </div>
       <div class="flex flex-col gap-5 text-dark-soft">
-        <div class="flex flex-col gap-[6px] max-w-70">
+        <div class="flex flex-col max-w-70">
           <h1 class="text-title text-black"><b>Embracing Minimalism</b></h1>
           <p class="text-body">From minimalist sculptures to minimalist paintings, this blog will inspire you to
             appreciate the beauty that
             lies in simplicity.</p>
         </div>
-        <p class="text-small">
+        <p class="text-small mt-5 mb-1">
           Annie Spratt
         </p>
       </div>
